79607d0b9045fff95ed30fbdaeb2cc5b1adb6ecc
Dmitrii Borisov <rundoom1337@gmail.com>
2023-05-08 22:21:43 +0700

    Holes almost fixed

--- GoblinNavigation.gd
+++ GoblinNavigation.gd
@@ -9,12 +9,25 @@ extends NavigationRegion2D
 func cut_polygon(occluder: CollisionObject2D):
 	var base_poly := Array(occluder.find_child("NavigationOccluder").polygon)
 	var offset_poly = base_poly.map(func(it): return it + occluder.global_position)
-	
+	var outline_array = []
 	for i in navigation_polygon.get_outline_count():
-		var poly = Geometry2D.clip_polygons(navigation_polygon.get_outline(i), offset_poly)
-
-		navigation_polygon.set_outline(i, poly[0])
-		if poly.size() == 2:
-			navigation_polygon.add_outline(poly[1])
+		if Geometry2D.is_polygon_clockwise(navigation_polygon.get_outline(i)):
+			outline_array.push_front(navigation_polygon.get_outline(i))
+		else:
+			outline_array.append(navigation_polygon.get_outline(i))
+	
+	navigation_polygon.clear_outlines()
+	
+	for i in outline_array.size():
+		var poly
+		if Geometry2D.is_polygon_clockwise(outline_array[i]):
+			printt(offset_poly, outline_array[i], !Geometry2D.intersect_polygons(outline_array[i], offset_poly).is_empty())
+		if Geometry2D.is_polygon_clockwise(outline_array[i]) and not Geometry2D.intersect_polygons(outline_array[i], offset_poly).is_empty():
+			poly = Geometry2D.merge_polygons(outline_array[i], offset_poly)
+			offset_poly = poly[0]
+		else:
+			poly = Geometry2D.clip_polygons(outline_array[i], offset_poly)
+			for it in poly:
+				navigation_polygon.add_outline(it)
 	
 	navigation_polygon.make_polygons_from_outlines()
